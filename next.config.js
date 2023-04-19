c0df58ab04ac52411b9c90c3d6aa3f0c64f42831
RaulSCoelho <84609153+RaulSCoelho@users.noreply.github.com>
2023-04-19 02:26:33 -0300

    Add Next.js app

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: '/raulscoelho.github.io',
+  assetPrefix: '/raulscoelho.github.io/',
   experimental: {
-    appDir: true,
-  },
+    appDir: true
+  }
 }
 
 module.exports = nextConfig
