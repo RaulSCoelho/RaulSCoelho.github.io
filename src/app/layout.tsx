e4d66829cc6192bb9999546b67b7b5fd68554619
RaulSCoelho <84609153+RaulSCoelho@users.noreply.github.com>
2023-04-24 10:42:14 -0300

    layout bg-color

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <head />
-      <body className="bg-app">
+      <body className="bg-[#7F7FD5] bg-app">
         <div className="h-screen p-20 flex items-center justify-center">
           <div className="overflow-hidden w-full h-full max-w-[1480px]">{children}</div>
         </div>
