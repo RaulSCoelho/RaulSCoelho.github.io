c30ad6c0379541eec7b6a4283b1a8c452b15b16d
RaulSCoelho <84609153+RaulSCoelho@users.noreply.github.com>
2023-04-24 12:29:23 -0300

    repos

--- src/app/repositories/page.tsx
+++ src/app/repositories/page.tsx
@@ -11,7 +11,7 @@ export default async function Repositories() {
   return (
     <div className="h-full flex flex-col">
       <h1 className="text-3xl font-bold mb-8">Repositories</h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 overflow-auto scrollbar scrollbar-thumb-[#ffffff4b] scrollbar-track-transparent">
+      <div className="px-2 pb-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 overflow-auto scrollbar scrollbar-thumb-[#ffffff4b] scrollbar-track-transparent">
         {repos?.map(repo => (
           <Repo repo={repo} key={repo.id} />
         ))}
