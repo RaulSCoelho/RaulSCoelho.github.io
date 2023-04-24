bd3d4249392ade777df313fed30b929d1abb19ff
RaulSCoelho <84609153+RaulSCoelho@users.noreply.github.com>
2023-04-24 11:33:58 -0300

    sorted repos

--- src/lib/repos.ts
+++ src/lib/repos.ts
@@ -3,5 +3,8 @@ import { Repository } from '@/dtos/repository'
 export async function getRepos() {
   const response = await fetch('https://api.github.com/users/RaulSCoelho/repos')
   const repos: Repository[] = await response.json()
+
+  repos.sort((a, b) => new Date(b.updated_at as string).getTime() - new Date(a.updated_at as string).getTime())
+
   return repos
 }
